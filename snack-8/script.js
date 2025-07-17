3a9866e3d38f3c8d9db3b34e3d90eb4a0f9dba6f
Steopa2001 <moraru495@gmail.com>
2025-07-17 15:55:27 +0200

    feat: end snack-8

--- snack-8/script.js
+++ snack-8/script.js
@@ -21,3 +21,13 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+const classe = students.find(function(studente) {
+  return studente.name === 'Marco Lanci';
+}).class;
+console.log(classe);
+
+// //arrow function
+// const classeMarco = students.find(student => student.name === 'Marco Lanci').class;
+
+// console.log(classeMarco); // '3C'
