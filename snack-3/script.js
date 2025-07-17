443e036963a18090e006d035880c89d10169a394
Steopa2001 <moraru495@gmail.com>
2025-07-17 15:03:07 +0200

    feat: end snack-3

--- snack-3/script.js
+++ snack-3/script.js
@@ -2,3 +2,14 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+
+//arrow function
+const incrementati = numbers.map(num => num + 1);
+
+console.log(incrementati);
+
+
+// //base function
+// const incrementati = numbers.map(function(num) {
+//   return num + 1;
+// });
